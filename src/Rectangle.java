d59ef55aa80c7138f90dd39780dc4d91d0a1b606
Pavel Saushkin <aceton41k@gmail.com>
2019-11-07 05:02:50 +0300

    4

---
+++ src/Rectangle.java
@@ -0,0 +1,38 @@
+public class Rectangle {
+    public int width, left, top, height;
+
+    public Rectangle(int height, int top, int left, int width) {
+         this.width = width;
+         this.height = height;
+         this.top = top;
+         this.left = left;
+    }
+
+    //Конструктор без параметров высота и ширина
+    public Rectangle(int top, int left) {
+        this.top = top;
+        this.left = left;
+    }
+
+    //Конструктор без параметра высота (берется от ширины, т.к. это квадрат).
+    public Rectangle(int top, int left, int width) {
+        this.height = width;
+        this.width = width;
+        this.top = top;
+        this.left = left;
+    }
+
+    //Конструктор, который принимает как параметр другой объект типа Rectangle
+
+    public Rectangle(Rectangle rectangle) {
+        this.height = rectangle.height;
+        this.top = rectangle.top;
+        this.left = rectangle.left;
+        this.width = rectangle.width;
+
+//        System.out.println(rectangle.height);
+
+    }
+
+
+}
